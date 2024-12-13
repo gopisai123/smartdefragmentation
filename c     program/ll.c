b75f6cf9c565031ebf828b1c32a5891e7e90353d
gopisai123 <gopijanga2006@gmail.com>
2024-12-14 00:20:26 +0530

    afdasf

--- c     program/ll.c
+++ c     program/ll.c
@@ -1,38 +1,38 @@
 //both int and pointer variable also takes 4 bytes 
-#include<stdio.h>
-#include<stdlib.h>
-struct Node {
-    int data;
-    struct Node* next; //in cpp just Node* link 
-};
-struct Node* head = NULL; // global value can be acessable to all those given functions 
-void insrt(int x);
-void pri();
-int main () {
-    int n,x;
-    scanf("%d",&n);
-    for(int i=0;i<n;i++) {
-        scanf("%d",&x);
-        insrt(x);
-        pri();
-    }
-    return 0;
-}
-void insrt(int x ) {
-    struct Node* temp =  (struct Node*)malloc (sizeof(struct Node));
-    temp -> data = x;
-    temp -> next = head;
-    head = temp ;
-}
-void pri() {
-    struct Node* val = head;
-    while(val!= NULL) {
-        printf("%d ",val-> data);
-        val = val->next;
+// #include<stdio.h>
+// #include<stdlib.h>
+// struct Node {
+//     int data;
+//     struct Node* next; //in cpp just Node* link 
+// };
+// struct Node* head = NULL; // global value can be acessable to all those given functions 
+// void insrt(int x);
+// void pri();
+// int main () {
+//     int n,x;
+//     scanf("%d",&n);
+//     for(int i=0;i<n;i++) {
+//         scanf("%d",&x);
+//         insrt(x);
+//         pri();
+//     }
+//     return 0;
+// }
+// void insrt(int x ) {
+//     struct Node* temp =  (struct Node*)malloc (sizeof(struct Node));
+//     temp -> data = x;
+//     temp -> next = head;
+//     head = temp ;
+// }
+// void pri() {
+//     struct Node* val = head;
+//     while(val!= NULL) {
+//         printf("%d ",val-> data);
+//         val = val->next;
 
-    }
+//     }
 
-}// refrence freecodecamp
+// }// refrence freecodecamp
 
 
 //insert node at nth position 
@@ -143,3 +143,20 @@ void pri() {
 // }
 
 
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    sum = num1 + num2;
+
+    printf("The sum is: %d\n", sum);
+
+    return 0;
+}
